8d53d04be1c24f4ed7910e21ad94e4e00c8a803b
Yarego Brozek <dev.kiloton@gmail.com>
2023-11-13 02:06:24 -0300

    Creating models

---
+++ src/modules/stocks/models/FuzzyAPI.ts
@@ -0,0 +1,4 @@
+export type FuzzyAPI = {
+  indexes: string[];
+  stocks: string[];
+};
---
+++ src/modules/stocks/models/StockAPI.ts
@@ -0,0 +1,39 @@
+export type StockAPI = {
+  results: Result[];
+  requestedAt: string;
+  took: string;
+};
+
+export type Result = {
+  symbol: string;
+  currency: string;
+  twoHundredDayAverage: number;
+  twoHundredDayAverageChange: number;
+  twoHundredDayAverageChangePercent: number;
+  marketCap: number;
+  shortName: string;
+  longName: string;
+  regularMarketChange: number;
+  regularMarketChangePercent: number;
+  regularMarketTime: string;
+  regularMarketPrice: number;
+  regularMarketDayHigh: number;
+  regularMarketDayRange: string;
+  regularMarketDayLow: number;
+  regularMarketVolume: number;
+  regularMarketPreviousClose: number;
+  regularMarketOpen: number;
+  averageDailyVolume3Month: number;
+  averageDailyVolume10Day: number;
+  fiftyTwoWeekLowChange: number;
+  fiftyTwoWeekLowChangePercent: any;
+  fiftyTwoWeekRange: string;
+  fiftyTwoWeekHighChange: number;
+  fiftyTwoWeekHighChangePercent: number;
+  fiftyTwoWeekLow: number;
+  fiftyTwoWeekHigh: number;
+  priceEarnings: number;
+  earningsPerShare: number;
+  logourl: string;
+  updatedAt: string;
+};
