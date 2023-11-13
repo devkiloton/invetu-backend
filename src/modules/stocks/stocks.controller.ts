e7b212a21991166528a04f2de85c12e148871d03
Yarego Brozek <dev.kiloton@gmail.com>
2023-11-12 21:07:27 -0300

    Creating stocks module

---
+++ src/modules/stocks/stocks.controller.ts
@@ -0,0 +1,32 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
+import { StocksService } from './stocks.service';
+import { CreateStockDto } from './dto/create-stock.dto';
+import { UpdateStockDto } from './dto/update-stock.dto';
+
+@Controller('stocks')
+export class StocksController {
+  constructor(private readonly stocksService: StocksService) {}
+
+  @Get()
+  findMany() {
+    return this.stocksService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.stocksService.findOne(+id);
+  }
+
+  @Get('dividends/:id')
+  findDividends(@Param('id') id: string) {
+    return this.stocksService.findDividends(+id);
+  }
+}
