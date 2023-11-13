622c6e891868529b895dd450b865d2e42f5f6a65
Yarego Brozek <dev.kiloton@gmail.com>
2023-11-12 21:05:54 -0300

    Removing unnecessary modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,7 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { StocksModule } from './modules/stocks/stocks.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [StocksModule],
 })
 export class AppModule {}
