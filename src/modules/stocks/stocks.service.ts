43ca819ec6b5afb33562bf15a96d7687bb2cd1ad
Yarego Brozek <dev.kiloton@gmail.com>
2023-11-13 02:10:18 -0300

    adding methods in the stocks service

--- src/modules/stocks/stocks.service.ts
+++ src/modules/stocks/stocks.service.ts
@@ -1,26 +1,58 @@
 import { Injectable } from '@nestjs/common';
-import { CreateStockDto } from './dto/create-stock.dto';
-import { UpdateStockDto } from './dto/update-stock.dto';
+import { ConfigService } from '@nestjs/config';
+import { FuzzyAPI } from './models/FuzzyAPI';
+import { HttpService } from '@nestjs/axios';
+import { firstValueFrom } from 'rxjs';
 
 @Injectable()
 export class StocksService {
-  create(createStockDto: CreateStockDto) {
-    return 'This action adds a new stock';
+  constructor(
+    private readonly configService: ConfigService,
+    private readonly httpService: HttpService,
+  ) {}
+  private readonly API = this.configService.get<string>('API_URL');
+  private readonly TOKEN = this.configService.get<string>('API_TOKEN');
+
+  async findAll(ids: string) {
+    const idsString = ids.split(',');
+    // run an await loop over the idsString array and each loop has 10 elements
+    const promises = [];
+
+    const asyncIterator = (async function* () {
+      // returns 10 elements at a time
+      for (let i = 0; i < idsString.length; i += 10) {
+        const ids = idsString.slice(i, i + 10);
+        yield ids;
+      }
+    })();
+    const stocksIterator = async () => {
+      for await (const value of asyncIterator) {
+        const response = await firstValueFrom(
+          this.httpService.get(
+            `${this.API}/quote/${value.join(',')}?token=${this.TOKEN}`,
+          ),
+        );
+        promises.push(response.data);
+      }
+    };
+    return stocksIterator().then(() => promises);
   }
 
-  findAll() {
-    return `This action returns all stocks`;
+  findOne(id: string) {
+    return;
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} stock`;
+  findDividends(id: string) {
+    return;
   }
 
-  update(id: number, updateStockDto: UpdateStockDto) {
-    return `This action updates a #${id} stock`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} stock`;
+  async findFuzzy(id: string): Promise<FuzzyAPI> {
+    const response = await firstValueFrom(
+      this.httpService.get(
+        `${this.API}/available?search=${id}&token=${this.TOKEN}"`,
+      ),
+    );
+    const data: FuzzyAPI = response.data;
+    return data;
   }
 }
