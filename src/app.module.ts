907d8ee34dab5743685be08061897b992bb48a24
Yarego Brozek <dev.kiloton@gmail.com>
2023-11-13 02:17:43 -0300

    Improving security

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,12 +4,19 @@ import { CryptosModule } from './modules/cryptos/cryptos.module';
 import { IndexesModule } from './modules/indexes/indexes.module';
 import { CurrenciesModule } from './modules/currencies/currencies.module';
 import { ConfigModule } from '@nestjs/config';
+import { ThrottlerModule } from '@nestjs/throttler';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
+    ThrottlerModule.forRoot([
+      {
+        ttl: 1000,
+        limit: 10,
+      },
+    ]),
     StocksModule,
     CryptosModule,
     IndexesModule,
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(helmet());
+  app.enableCors({
+    origin: [process.env.LOCAL_CLIENT, process.env.PROD_CLIENT],
+  });
   await app.listen(3000);
 }
 bootstrap();
