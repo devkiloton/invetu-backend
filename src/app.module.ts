fa197b571cf4f2f103174fc323030827abbd4c4f
Yarego Brozek <dev.kiloton@gmail.com>
2023-11-13 02:07:08 -0300

    Setting up config module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,8 +3,17 @@ import { StocksModule } from './modules/stocks/stocks.module';
 import { CryptosModule } from './modules/cryptos/cryptos.module';
 import { IndexesModule } from './modules/indexes/indexes.module';
 import { CurrenciesModule } from './modules/currencies/currencies.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [StocksModule, CryptosModule, IndexesModule, CurrenciesModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    StocksModule,
+    CryptosModule,
+    IndexesModule,
+    CurrenciesModule,
+  ],
 })
 export class AppModule {}
