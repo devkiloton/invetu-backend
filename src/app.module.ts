be9ff5967454b409f70b16c5cb19e68406849156
Yarego Brozek <dev.kiloton@gmail.com>
2023-11-12 21:11:12 -0300

    Creating currencies module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,9 @@ import { Module } from '@nestjs/common';
 import { StocksModule } from './modules/stocks/stocks.module';
 import { CryptosModule } from './modules/cryptos/cryptos.module';
 import { IndexesModule } from './modules/indexes/indexes.module';
+import { CurrenciesModule } from './modules/currencies/currencies.module';
 
 @Module({
-  imports: [StocksModule, CryptosModule, IndexesModule],
+  imports: [StocksModule, CryptosModule, IndexesModule, CurrenciesModule],
 })
 export class AppModule {}
