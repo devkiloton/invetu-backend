8e52510bace538cea0b7a84c51b011907c070c4c
Yarego Brozek <dev.kiloton@gmail.com>
2023-11-13 02:09:58 -0300

    setting up http module

--- src/modules/stocks/stocks.module.ts
+++ src/modules/stocks/stocks.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { StocksService } from './stocks.service';
 import { StocksController } from './stocks.controller';
+import { HttpModule } from '@nestjs/axios';
 
 @Module({
   controllers: [StocksController],
   providers: [StocksService],
+  imports: [HttpModule],
 })
 export class StocksModule {}
