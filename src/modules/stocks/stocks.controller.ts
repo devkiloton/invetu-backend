e6631a5d6c188d9d2256133709c914d5d4cf45ff
Yarego Brozek <dev.kiloton@gmail.com>
2023-11-13 02:09:48 -0300

    Improving stocks controller

--- src/modules/stocks/stocks.controller.ts
+++ src/modules/stocks/stocks.controller.ts
@@ -1,32 +1,27 @@
-import {
-  Controller,
-  Get,
-  Post,
-  Body,
-  Patch,
-  Param,
-  Delete,
-} from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { StocksService } from './stocks.service';
-import { CreateStockDto } from './dto/create-stock.dto';
-import { UpdateStockDto } from './dto/update-stock.dto';
 
 @Controller('stocks')
 export class StocksController {
   constructor(private readonly stocksService: StocksService) {}
 
-  @Get()
-  findMany() {
-    return this.stocksService.findAll();
+  @Get('/:ids')
+  findAll(@Param('ids') ids: string) {
+    return this.stocksService.findAll(ids);
   }
 
-  @Get(':id')
+  @Get('/:id')
   findOne(@Param('id') id: string) {
-    return this.stocksService.findOne(+id);
+    return this.stocksService.findOne(id);
   }
 
   @Get('dividends/:id')
   findDividends(@Param('id') id: string) {
-    return this.stocksService.findDividends(+id);
+    return this.stocksService.findDividends(id);
+  }
+
+  @Get('fuzzy/:id')
+  findFuzzy(@Param('id') id: string) {
+    return this.stocksService.findFuzzy(id);
   }
 }
